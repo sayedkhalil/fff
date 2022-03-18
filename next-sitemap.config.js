1b2cda92db8f0a3edfe46139d15aafdfa18949fe
sayedkhalil <sayedkhalil@gmail.com>
2022-03-18 05:03:17 +0300

    sitemap1

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+    siteUrl:"https://elfursan.com" ,
+    generateRobotsTxt: true,
+    sitemapSize: 1000,
+
+  }
--- next-sitemap.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    siteUrl:'https://elfursan.com',
-    generateRobotsTxt: true,
-  }
